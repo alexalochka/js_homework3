becd305fbdeb2bdd893b759381d7122301b3a70c
alexalochka <veysts@gmail.com>
2025-02-06 15:56:58 +0200

    dz

--- js_dz_3.js
+++ js_dz_3.js
@@ -18,35 +18,4 @@ console.log(c)
 
 
 
-// Other working schemes but does not meet requirements fully
-
-
-
-// function so(a) {
-//     return a * a * a;
-// };
-// let result = so(4);
-// console.log(result);
-
-
-
-
-// function fn(a, b) {
-//     return b ? a * fn(a, b - 1) : 1;
-// };
-
-// console.log(fn(4, 3));
-
-
-
-
-// let number = 4;
-// // let k;
-// for (let q = 3; q<=16; q++){
-//     if ((number * q) !== 64) continue;
-//     // return result = (number * q);
-//     console.log(number*q);
-// }
-
-
 
